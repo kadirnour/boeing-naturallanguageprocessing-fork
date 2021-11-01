a7bca482928718cdac9d2f9532e566eb159f705e
Riley Hunter <riley93hunter@gmail.com>
2021-11-01 16:07:56 -0700

    fixed timer

--- Parser/main.py
+++ Parser/main.py
@@ -4,16 +4,19 @@ import argparse
 from Parser import Spacy
 #import database
 #import csv
-#import time
+import time
 # from Taxonomy import
 from Parser import noun as Noun
 import sys
 from Parser import output_writers
 #import unit_tests
+#import parsers
 
 import os
 from Parser import NLTK
 from pathlib import Path
+
+#import noun
 
 # Removed, use our program launcher instead and enter locations through command line
 # def dir_path(path):
@@ -22,12 +25,12 @@ from pathlib import Path
 #     else:
 #         raise argparse.ArgumentTypeError(f"readable_dir:{path} is not a valid path")
 
-# def parse_args(args):
-#     parser = argparse.ArgumentParser()
-#     # parser.add_argument('--file', '-f', type=str, required=True, nargs='+', help='path to file to parse')
-#     parser.add_argument('--path', '-f', type=dir_path, required=True, nargs='+', help='path to file to parse')
-#     # parser.add_argument('--database', '-d', action='store_true', required=False, help='triggers the use of a mysql database')
-#     return parser.parse_args()
+#def parse_args(args):
+#    parser = argparse.ArgumentParser()
+#    parser.add_argument('--file', '-f', type=str, required=True, nargs='+', help='path to file to parse')
+#    parser.add_argument('--path', '-f', type=dir_path, required=True, nargs='+', help='path to file to parse')
+#    parser.add_argument('--database', '-d', action='store_true', required=False, help='triggers the use of a mysql database')
+#    return parser.parse_args()
 
 
 def main():
@@ -36,25 +39,38 @@ def main():
     #path = parse_args(sys.argv).path[0]
     # Gets path from assumed position instead of args
 
-    # path = os.getcwd() + '\\Parser\\data'
-    # folder = os.listdir(path)
+    #path = os.getcwd() + '\\Parser\\data'
+    #folder = os.listdir(path)
 
     data = Path('Parser/data')
 
     for file in data.iterdir():
         #docInfo, total_nouns, total_sentences = parsers.run_parsers(file)
 
+        #start timer
+        startTime = time.time()
+
         # parse file
         docInfo, total_nouns, total_sentences = Spacy.run_parsers(file)
         #docInfo, total_nouns, total_sentences = NLTK.run_parsers(file_path)
+
+        # #     # end timer
+        elapsedTime = time.time() - startTime
+        totalTimeStr = "Total time: " + str(round(elapsedTime, 3)) + " sec" # used in .csv file
+
 
         # calculate unique nouns, total nouns
         unqNouns = len(total_nouns)
         sumNouns = Noun.calculate_num_nouns_occur(total_nouns)
 
         # placeholders for now, need to fix
-        totalTimeStr = "Total time: Uncomputed"
-        costPerNounStr = "Cost per noun: Uncomputed"
+        # totalTimeStr = "Total time: Uncomputed"
+        # costPerNounStr = "Cost per noun: Uncomputed"
+
+    # calculate cost per noun in milliseconds
+        costPerNoun = (elapsedTime * 1000) / sumNouns
+        costPerNounStr = "Cost per noun: " + str(round(costPerNoun, 3)) + " ms" # used in .csv file
+
 
         output_writers.to_csv(
             docInfo, totalTimeStr, costPerNounStr, total_nouns, unqNouns, sumNouns, file)
@@ -77,27 +93,27 @@ def main():
     #         docInfo, totalTimeStr, costPerNounStr, total_nouns, unqNouns, sumNouns, file)
 
     # Removed, old code, we need to fix the timer
-    # args = parse_args(sys.argv[1:])
+    #args = parse_args(sys.argv[1:])
 
-    # for file_path in args.file:
-    #     if not parsers.validate_file(file_path):
-    #         continue
+    #for file_path in args.file:
+    #    if not parsers.validate_file(file_path):
+    #        continue
 
-    #     # start timer
-    #     startTime = time.time()
+         # start timer
+     #   startTime = time.time()
 
     #     # parse file
-    #     docInfo, total_nouns, total_sentences = parsers.run_parsers(file_path)
+    #    docInfo, total_nouns, total_sentences = parsers.run_parsers(file_path)
 
-    #     # end timer
+    # #     # end timer
     #     elapsedTime = time.time() - startTime
     #     totalTimeStr = "Total time: " + str(round(elapsedTime, 3)) + " sec" # used in .csv file
 
-    #     # calculate unique nouns, total nouns
+    # #     # calculate unique nouns, total nouns
     #     unqNouns = len(total_nouns)
     #     sumNouns = noun.calculate_num_nouns_occur(total_nouns)
 
-    #     # calculate cost per noun in milliseconds
+    # #     # calculate cost per noun in milliseconds
     #     costPerNoun = (elapsedTime * 1000) / sumNouns
     #     costPerNounStr = "Cost per noun: " + str(round(costPerNoun, 3)) + " ms" # used in .csv file
 
