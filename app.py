2c4bf38a66ea293ff49589e2974f68efe93d563d
Riley Hunter <riley93hunter@gmail.com>
2021-11-10 21:16:34 -0800

    reimplemented weights/freq

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from Parser import main as parser
+from Taxonomy import extraction
 
 # Can interact with out code and old code
 
 def main():
     parser.main()
+
+    parsed_terms = extraction.Parsing.extract_frequencies_from_csv()
+    print(parsed_terms)
 
 if __name__ == "__main__":
     main()
