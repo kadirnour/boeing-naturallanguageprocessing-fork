ffce03bd45a5ea913997eb0392d0b5d1fe256885
Kadir <kadir.nour@wsu.edu>
2021-11-10 20:41:26 -0800

    Beginning implementation of docx extraction.

--- Parser/text_replacer.py
+++ Parser/text_replacer.py
@@ -11,27 +11,34 @@ TODO: find alternative to if statements
 
 '''''''''''''''''''''''''''''''''''''''''''''''
 
-def text_replacer(text):
-    
-    if text.find("\\xe2\\x80\\x99")!=-1:
-        text = text.replace("\\xe2\\x80\\x99", "'")
-    
-    if text.find("\\xe2\\x80\\x9c")!=-1:
-        text = text.replace("\\xe2\\x80\\x9c", "\"")
-    
-    if text.find("\\xe2\\x80\\x9d")!=-1:
-        text = text.replace("\\xe2\\x80\\x9d","\"")
 
-    if text.find("\\'")!=-1:
+def text_replacer(text):
+
+    if text.find("\\xe2\\x80\\x99") != -1:
+        text = text.replace("\\xe2\\x80\\x99", "'")
+
+    if text.find("\\xe2\\x80\\x9c") != -1:
+        text = text.replace("\\xe2\\x80\\x9c", "\"")
+
+    if text.find("\\xe2\\x80\\x90") != -1:
+        text = text.replace("\\xe2\\x80\\x90", "-")
+
+    if text.find("\\xe2\\x80\\xa6") != -1:
+        text = text.replace("\\xe2\\x80\\xa6", "...")
+
+    if text.find("\\xe2\\x80\\x9d") != -1:
+        text = text.replace("\\xe2\\x80\\x9d", "\"")
+
+    if text.find("\\'") != -1:
         text = text.replace("\\'", "'")
 
-    if text.find("b'")!=-1:
+    if text.find("b'") != -1:
         text = text.replace("b'", "")
 
-    if text.find('\\n')!=-1:
+    if text.find('\\n') != -1:
         text = text.replace('\\n', '')
 
-    if text.find("b\"")!=-1:
+    if text.find("b\"") != -1:
         text = text.replace("b\"", "")
 
     return text
