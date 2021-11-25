10dfccefa7dbda99939964e2968faaeb9cbe51ee
Brandon-LC <brandonlcode@gmail.com>
2021-11-24 16:00:39 -0800

    Back end and front end communicating

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from Parser import main as parser
 from Taxonomy import extraction
 from tests import unit_tests
+from flask import Flask
 
 '''''''''''''''''''''''''''''''''''''''''''''''''''
 Function: main
@@ -8,16 +9,23 @@ Description: main, runs all functions
 Parameters:
 Returns:
 '''''''''''''''''''''''''''''''''''''''''''''''''''
-def main():
-    # STEP 1: Run parser and extract terms from documents
-    parser.parse()
+# def main():
+    # # STEP 1: Run parser and extract terms from documents
+    # parser.parse()
 
-    # STEP 2: Find frequencies and weights of terms
-    parsed_terms = extraction.find_frequencies_and_weights()
-    print(parsed_terms)
+    # # STEP 2: Find frequencies and weights of terms
+    # parsed_terms = extraction.find_frequencies_and_weights()
+    # print(parsed_terms)
 
-    unit_tests.test_accuracy()
+    # unit_tests.test_accuracy()
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+app = Flask(__name__)
+
+@app.route('/parser')
+def parse():
+    print("HERE")
+    return "HELLO WORLD!"
