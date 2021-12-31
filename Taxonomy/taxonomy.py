1f3e45b532e73e5f17c34f28cdd4eba667d8c0cf
Riley Hunter <riley93hunter@gmail.com>
2021-12-30 18:57:40 -0800

    Skeleton code for tax reader

---
+++ Taxonomy/taxonomy.py
@@ -0,0 +1,38 @@
+import csv
+import os
+
+'''
+Method: Taxonomy Reader
+Parameters:
+Input/Ouput:
+Descriptions: Reads the data off the csv
+'''
+def taxonomyReader():
+    pass
+
+'''
+Method: Taxonomy Calculator
+Parameters:
+Input/Ouput:
+Descriptions: Calculates the weights of each category 
+'''
+def taxonomyCalculator():
+    pass
+
+'''
+Method:
+Parameters:
+Input/Ouput:
+Descriptions: Processes the taxonomy into a dictionary
+'''
+def taxonomyProcessor():
+    pass
+
+'''
+Method:
+Parameters:
+Input/Ouput:
+Descriptions: Sends the taxonomy data to the flask route
+'''
+def taxonomySender():
+    pass
