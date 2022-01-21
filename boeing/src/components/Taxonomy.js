1713bd7d2f98f9058eb5ff2c4fe9ec35f1626c48
Brandon-LC <89604738+Brandon-LC@users.noreply.github.com>
2022-01-20 17:13:41 -0800

    added vis.js

--- boeing/src/components/Taxonomy.js
+++ boeing/src/components/Taxonomy.js
@@ -1,32 +1,96 @@
 import React from 'react';
-import example from '../images/taxonomy_example.png';
+//import example from '../images/taxonomy_example.png';
+
+import Graph from 'vis-react';
+
+var options = {
+    layout: {
+        hierarchical: true
+    },
+    edges: {
+        color: '#000000'
+    },
+    interaction: { hoverEdges: true, multiselect: true, hover: true}
+};
+
+
 
 class Taxonomy extends React.Component {
+    constructor() {  
+        super();  
+        this.state = {  
+            newID: 0,
+            counter: 5,
+            graph: {
+              nodes: [
+                { id: 1, label: "Node 1", color: "#e04141" },
+                { id: 2, label: "Node 2", color: "#e09c41" },
+                { id: 3, label: "Node 3", color: "#e0df41" },
+                { id: 4, label: "Node 4", color: "#7be041" },
+                { id: 5, label: "Node 5", color: "#41e0c9" }
+              ],
+              edges: [
+                { from: 1, to: 2 },
+                { from: 1, to: 3 },
+                { from: 2, to: 4 },
+                { from: 2, to: 5 }
+              ]
+            },
+            events: {
+              click: ({ nodes, edges }) => {
+                  this.setState({nodes: nodes})
+                //this.doSomething2(nodes, edges)
+                // console.log("Selected nodes:");
+                // console.log(nodes);
+                // console.log("Selected edges:");
+                // console.log(edges);
+                // alert("Selected node: " + nodes);
+              }
+            }
+        }  
+    } 
 
-     
+    doSomething = () => {
+        let counter = this.state.counter + 1
+        let newNodes = [...this.state.graph.nodes]
+        let newGraph = {...this.state.graph}
+
+        newNodes.push({id: counter, label: `Node ${counter}`, color: '#e04141'})
+
+        newGraph.nodes = newNodes
+
+        this.setState({
+            counter: counter,
+            graph: newGraph,
+            newID: this.state.newID + 1
+        })         
+    }
+
+    doSomething2 = () => {
+        let newEdges = [...this.state.graph.edges]
+        let newGraph = {...this.state.graph}
+
+        newEdges.push({from: this.state.nodes[0], to: this.state.nodes[1]})
+
+        newGraph.edges = newEdges
+
+        this.setState({
+            graph: newGraph,
+            newID: this.state.newID + 1
+        })            
+    }
+
 
     render() {
         return (
-            <div className="page">
-                <h2 className="pageTitle"> Step 4: Taxonomy </h2>
-                <div className="pageBox">
-                <h6> Under Construction </h6>
-                <div className="modeBtn">
-                            <button className="btn bottom6" onClick={() => this.generateTaxonomy()}> generateTaxonomy </button>
-                        </div>
-                    <div className="categoriesUploadSection2">
-                        <div>
-                            <img className="example" src={example}/>
-                        </div>    
-                        <div className="modeBtn">
-                            <button className="btn bottom6" onClick={() => this.props.prevPage()}> Back </button>
-                        </div>
-                    </div>
-                </div>
+            <div className = "page">
+                <button onClick={() => this.doSomething()}>CLICK ME</button>
+                <button onClick={() => this.doSomething2()}>CLICK ME 2</button>
+                <Graph key={this.state.newID} graph={this.state.graph} options={options} events={this.state.events} style={{ height: "640px" }} />
             </div>
         )
     }
+
 }
 
-
-export default Taxonomy;
+export default Taxonomy
