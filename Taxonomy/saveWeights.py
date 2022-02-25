33951653430f98c68a58a7d209ab18d504f7d8d5
Riley Hunter <riley.hunter@wsu.edu>
2022-02-25 14:45:14 -0800

    got the weights data in rows

--- Taxonomy/saveWeights.py
+++ Taxonomy/saveWeights.py
@@ -1,4 +1,23 @@
+#Saves weights from state to csv everytime button is pressed to reflect changes#
+
+
 def saveWeight(folder,corpusName,weight):
-    print(folder)
-    print(corpusName)
-    print(weight)
+    #print(folder)
+    #print(corpusName)
+    file = folder + '\\' + corpusName + '.csv' # csv from folder
+    print(file)
+    #print(weight)
+    weightWrite = []
+    weightWrite = processDict(weight)
+
+
+def processDict(weight):
+    weightWrite = []
+    for term,data in weight.items():
+        rowData = []
+        rowData.append(term)
+        for field,value in data.items():
+            rowData.append(value)
+        weightWrite.append(rowData)
+    print(weightWrite)
+    return weightWrite
