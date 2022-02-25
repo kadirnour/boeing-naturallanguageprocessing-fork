7c25bffaeeb87f2300e6ca42450306b75b3c7e55
Riley Hunter <riley.hunter@wsu.edu>
2022-02-25 15:14:26 -0800

    done with deletesgit add . git add .

--- Taxonomy/saveWeights.py
+++ Taxonomy/saveWeights.py
@@ -1,23 +1,45 @@
 #Saves weights from state to csv everytime button is pressed to reflect changes#
 
 
+from csv import reader
+import csv
+
+
 def saveWeight(folder,corpusName,weight):
     #print(folder)
     #print(corpusName)
     file = folder + '\\' + corpusName + '.csv' # csv from folder
     print(file)
-    #print(weight)
-    weightWrite = []
-    weightWrite = processDict(weight)
-
+    #weightReader.append(next(csv_reader))
+    weightWriter = processDict(weight)
+    weightWrite(weightWriter,file)
 
 def processDict(weight):
-    weightWrite = []
+    weightReader = []
     for term,data in weight.items():
         rowData = []
         rowData.append(term)
         for field,value in data.items():
             rowData.append(value)
-        weightWrite.append(rowData)
-    print(weightWrite)
-    return weightWrite
+        weightReader.append(rowData)
+    print(weightReader)
+    return weightReader
+
+
+def weightWrite(weightWriter,file):
+    with open(file, 'w', newline='') as write_obj:
+        csv_writer = csv.writer(write_obj)
+        for row in weightWriter:
+            csv_writer.writerow(row)
+
+'''
+def data_writer(data_write, file_path):
+    with open(file_path, 'r') as read_obj, \
+        open(file_path, 'w', newline='') as write_obj:
+        
+        # Create a csv.writer object from the output file object
+        csv_writer = csv.writer(write_obj)
+        
+        # Read each row of the input csv file as list
+        for row in data_write:
+            csv_writer.writerow(row)'''
